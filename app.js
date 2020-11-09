bc145c7e6fa89c9752f6499ce57b9129b4f174fe
Paul Thomas <paulthomas2107@gmail.com>
2020-11-09 12:57:11 +0000

    First...

---
+++ app.js
@@ -0,0 +1,100 @@
+// Scores
+let userScore = 0;
+let computerScore = 0;
+
+//DOM Variables - cache DOM
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+// Functions
+function getComputerChoice() {
+  const choices = ["r", "p", "s"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+function convertChoiceToType(choice) {
+  let types = new Map();
+  types.set("r", "Rock");
+  types.set("p", "Paper");
+  types.set("s", "Scissors");
+  return types.get(choice);
+}
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertChoiceToType(
+    userChoice
+  )}${smallUserWord} beats ${convertChoiceToType(
+    computerChoice
+    )}${smallCompWord}. You Win 🔥`;
+  
+}
+
+function fail(userChoice, computerChoice) {
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertChoiceToType(
+    userChoice
+  )}${smallUserWord} loses to ${convertChoiceToType(
+    computerChoice
+  )}${smallCompWord}. You Lost 💩`;
+}
+
+function draw(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertChoiceToType(
+    userChoice
+  )}${smallUserWord} equals ${convertChoiceToType(
+    computerChoice
+  )}${smallCompWord}. You Draw 😄`;
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      fail(userChoice, computerChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw(userChoice, computerChoice);
+      break;
+  }
+}
+
+function main() {
+  // Event listeners
+  rock_div.addEventListener("click", function () {
+    game("r");
+  });
+  paper_div.addEventListener("click", function () {
+    game("p");
+  });
+  scissors_div.addEventListener("click", function () {
+    game("s");
+  });
+}
+
+main();
